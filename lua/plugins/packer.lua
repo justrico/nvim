434dcbb43029549abb30b7facd0e581976d00410
justrico <ricoyang9@163.com>
2023-01-27 19:59:36 +0800

    add nvim-tree

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -8,17 +8,19 @@ return require('packer').startup(function(use)
     use 'kyazdani42/nvim-web-devicons'
     --use 'rcarriga/nvim-notify'
 
-    use {
-        'nvim-lualine/lualine.nvim',
+    use { 'nvim-lualine/lualine.nvim',
         requires = { 'kyazdani42/nvim-web-devicons', opt = true }
     }
 
-    use {
-        'akinsho/bufferline.nvim', tag = "v3.*",
+    use { 'akinsho/bufferline.nvim', tag = "v3.*",
         requires = 'nvim-tree/nvim-web-devicons'
     }
 
-    -- catppuccin for neovim
     use { "catppuccin/nvim", as = "catppuccin" }
+
+    use { 'nvim-tree/nvim-tree.lua',
+        requires = 'nvim-tree/nvim-web-devicons', -- optional, for file icons
+        -- tag = 'nightly' -- optional, updated every week. (see issue #1193)
+    }
 end)
 
