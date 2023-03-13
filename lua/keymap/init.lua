716c828fb72884a28744898afbb63ac1d746a67b
justrico <ricoyang9@163.com>
2023-03-13 23:12:51 +0800

    change move cursor keymaps with arrow keys in insert mode

--- lua/keymap/init.lua
+++ lua/keymap/init.lua
@@ -54,10 +54,10 @@ local basic_map = {
     ['x|S'] = mapcr('w'):noremap():silent(),
     ['x|Q'] = mapcr('BufferLinePickClose'):noremap():silent(),
     -- insert mode
-    ['i|<A-u>'] = map('<Esc>ka'):noremap():silent(),
-    ['i|<A-e>'] = map('<Esc>ja'):noremap():silent(),
-    ['i|<A-n>'] = map('<Esc>ha'):noremap():silent(),
-    ['i|<A-i>'] = map('<Esc>la'):noremap():silent(), -- DO NOT use <C-i>, it's same as <Tab> on terminal
+    ['i|<A-u>'] = map('<up>'):noremap():silent(),
+    ['i|<A-e>'] = map('<down>'):noremap():silent(),
+    ['i|<A-n>'] = map('<left>'):noremap():silent(),
+    ['i|<A-i>'] = map('<right>'):noremap():silent(), -- DO NOT use <C-i>, it's same as <Tab> on terminal
 }
 
 bind.nvim_load_mapping(basic_map)
